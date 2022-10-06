ad1a5c3467a6225d47b6511932140d2397e1f1e1
Natalia <natalia-gasiorowska@o2.pl>
2022-10-06 20:43:03 +0200

    Proxy p5

--- src/proxy/dynamic/DatingService.java
+++ src/proxy/dynamic/DatingService.java
@@ -8,11 +8,29 @@ public class DatingService {
 
     public static void main(String[] args) {
         PersonComponent p1 = new PersonComponentImpl("P 1", "M", "Sport");
-        PersonComponent owner = getOwnerProxy(p1);
-        PersonComponent stranger = getStrangerProxy(p1);
 
+        PersonComponent owner = getOwnerProxy(p1);
         System.out.println("Owner - get name: " + owner.getName());
-        //502
+        owner.setHobby("Cooking");
+        System.out.println("Owner has set hobby");
+
+        try {
+            owner.setBeautyRank(9);
+            System.out.println("Owner has set beauty rank");
+        } catch (Exception e) {
+            System.out.println("Owner has NOT set beauty rank");
+        }
+
+        PersonComponent stranger = getStrangerProxy(p1);
+        System.out.println("Stranger - get name: " + stranger.getName());
+        try {
+            stranger.setHobby("Music");
+            System.out.println("Stranger has set hobby");
+        } catch (Exception e) {
+            System.out.println("Stranger has NOT set hobby");
+        }
+        stranger.setBeautyRank(3);
+        System.out.println("Stranger has set beauty rank");
     }
 
     private static PersonComponent getOwnerProxy(PersonComponent person) {
